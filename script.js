52ccfa783589a1f1444a62bcb394d77b42ae15f4
Nazar_Panyushchyk <npanusik@gmail.com>
2024-04-28 18:27:20 +0300

    add

---
+++ script.js
@@ -0,0 +1,41 @@
+// task1
+const button = document.querySelector('#buttonClick');
+console.log(button);
+
+const input = document.querySelector('#form');
+console.log(input);
+
+button.addEventListener('click', function() {
+    const newText = input.value;
+    button.innerHTML = newText;
+})
+
+// task2
+const img = document.querySelector('.image')
+console.log(img);
+
+const image = document.querySelector('.image');
+image.src = './img/planetimg.jpg';
+
+// task3
+const a = document.querySelector('.link');
+console.log(a);
+
+const ahref = document.querySelector('.link');
+ahref.href = 'https://www.w3schools.com/tags/att_a_href.asp';
+
+const picture = document.querySelector('.picture');
+console.log(picture);
+
+const pictures = document.querySelector('.picture');
+pictures.alt = 'Новий опис зображення';
+
+// task4
+const list = document.querySelector('.list');
+console.log(list);
+
+const firstElement = document.querySelector('li:first-child');
+firstElement.textContent = 'new text';
+
+
+
